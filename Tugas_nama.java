163801f1fea495bbfec860406f89300bcc0741ec
mzakariap3 <mzakariap3@gmail.com>
2024-09-20 15:57:54 +0700

    Tugas Bu Rizza

---
+++ Tugas_nama.java
@@ -0,0 +1,7 @@
+class helloworld{
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad Zakaria Prasetyo");
+        System.out.println("Nim: 607062400062");
+        System.out.println("Kelas: D3IF - 48 - 01");
+    }
+}
